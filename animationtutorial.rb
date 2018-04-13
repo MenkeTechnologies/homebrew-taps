88cde03d2522b823ebe32ce7e986b43a6d3ac792
Jacob Menke <jamenk@email.wm.edu>
2018-04-13 12:08:28 -0400

    depends on

--- animationtutorial.rb
+++ animationtutorial.rb
@@ -1,15 +1,15 @@
 class Animationtutorial < Formula
-  desc "Cool animation"
-  homepage "https://github.com/MenkeTechnologies/AnimationTutorial"
-  url "https://github.com/MenkeTechnologies/AnimationTutorial/archive/v1.0.0.tar.gz"
-  sha256 "d5098aa2b5ee659cd803d7298fad491623c31c53530a5bd9e5b4e65e44dbd771"
+    desc "Cool animation"
+    homepage "https://github.com/MenkeTechnologies/AnimationTutorial"
+    url "https://github.com/MenkeTechnologies/AnimationTutorial/archive/v1.0.0.tar.gz"
+    sha256 "d5098aa2b5ee659cd803d7298fad491623c31c53530a5bd9e5b4e65e44dbd771"
 
-  depends_on "bash" => :run
+    depends_on :formula => "bash"
 
-  def install
-  	bin.install "bin/animation"
-  end
+    def install
+        bin.install "bin/animation"
+    end
 
-  test do
-  end
+    test do
+    end
 end
